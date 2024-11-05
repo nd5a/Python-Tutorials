addb0ad51942a9d4f709e1d0313708920bcb9592
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2024-11-05 11:05:37 +0530

    Day 1: Python Basics

---
+++ Day1.py
@@ -0,0 +1,41 @@
+# print("Hello World!\nHello World!\nHello World!")
+
+# print("Hello"+" "+"Dhruvil")
+
+# print("Day 1- String Manipulation")
+# print("String Concatation is done with the "+"sign.")
+# print('e.g. print ("Hello"+ "World")')
+# print("New Lines can be created with a backslash and n.")
+
+#input() will be get user input in console
+# then print() will print the word "Hello" and the user input.
+# print("Hello "+input("What is Your Name?"))
+
+# print(len("Dhruvil"))
+# print(len(input("What is Yor Name ?")))
+
+# name = input("What is Your Name?")
+# print("Hello "+name)
+
+# name = input("What is Your Name ? ")
+# length = len(name)
+# print(length)
+
+# Swap Two Numbers
+
+# a=input("a: ")
+# b=input("b: ")
+# print("a =",a,"b =",b)
+
+# c=a
+# a=b
+# b=c
+# print("a =",a,"b =",b)
+
+print("Welcome to the band Game Generator\n")
+
+city=(input("Which City did you Grow up in ?\n"))
+
+pet=input("What is the name of a pet? \n")
+
+print("Your band name could be "+city+" "+pet)
