35fda6464868a6f939a6140dbe70c5a27ff36c21
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2024-12-03 18:02:22 +0530

    Day 14: Higher - Lower Game Mini Project

---
+++ Day14.py
@@ -0,0 +1,264 @@
+# Higher or lower game
+
+data = [    {
+        'name': 'Instagram',
+        'follower_count': 172,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 145,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 133,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 131,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 127,
+        'description': 'Reality TV personality and Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 119,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 113,
+        'description': 'Musician',
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Nike',
+                'follower_count': 109,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ellen DeGeneres',
+        'follower_count': 87,
+        'description': 'Comedian',
+        'country': 'United States'
+    },
+    {
+        'name': 'Real Madrid CF',
+        'follower_count': 86,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 85,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Rihanna',
+        'follower_count': 81,
+        'description': 'Musician and businesswoman',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'Demi Lovato',
+        'follower_count': 80,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': "Victoria's Secret",
+        'follower_count': 69,
+        'description': 'Lingerie brand',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 68,
+        'description': 'Actress and musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 66,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 65,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Chris Brown',
+        'follower_count': 64,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 63,
+        'description': 'Basketball player',
+        'country': 'United States'
+    },    {
+        'country': 'Canada'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 55,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Gigi Hadid',
+        'follower_count': 54,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 53,
+        'description': 'Actress and musician',
+        'country': 'India'
+    },
+    {
+        'name': '9GAG',
+        'follower_count': 52,
+        'description': 'Social media platform',
+        'country': 'China'
+    },
+    {
+        'name': 'Ronaldinho',
+        'follower_count': 51,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'Maluma',
+        'follower_count': 50,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 49,
+        'description': 'Musician',
+        'country': 'Cuba'
+    },
+    {
+        'name': 'NBA',
+        'follower_count': 47,
+        'description': 'Club Basketball Competition',
+        'country': 'United States'
+    }
+]
+
+logo = """
+    __  ___       __             
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /    
+/_/ ///_/\__, /_/ /_/\___/_/     
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /    
+/_____/\____/|__/|__/\___/_/     
+"""
+
+vs = """
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+"""
+import random   
+from replit import clear
+
+def get_random_account():
+    return random.choice(data)
+
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    
+    return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
+            
